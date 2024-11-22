92f9f01404f4c3d7eac81b7bb7f498caf23b0b44
Andrew Tamburino <57277787+atamburino@users.noreply.github.com>
2024-11-21 23:07:26 -0500

    refactored following code to be a level higher at the app level to allow us to follow all posts for that user

--- src/InstagramPost.js
+++ src/InstagramPost.js
@@ -92,34 +92,49 @@ class InstagramPost extends Component {
     render() {
         console.log(`User ${this.state.userName} isVerified:`, this.state.isVerified);
         return (
-            <div className="post">
-                <div className="post-header">
-                    <img src={`./images/user/${this.state.userImgURL}`} alt="User Profile" className="profile-pic" />
-                    {/* Task 5: Make the <h2> below dynamically reference the dummy user name. */}
-                    <h2>{this.state.userName}</h2>
-                    {this.state.isVerified && <VerifiedBadge />}
-                    {!this.state.isUser && <FollowButton following={this.state.following} />}
-                </div>
-                {/* Task 6: For the element below, pattern match the src value referencing the user profile image src above. Refer to the insta.css file and also add the appropriate class name */}
-                <img src={`./images/post/${this.state.postImgURL}`} alt="Post Image" className="post-image"/>
-                <div className="post-actions">
-                    <PostActions 
-                        totalLikes={this.state.totalLikes} 
-                        liked={this.state.liked}
-                        saved={this.state.saved}
-                        totalComments={this.state.totalComments}
-                        shares={this.state.shares}
-                        // Task 7: Pass down the appropriate handler callback functions the child component will need to invoke when/if things are clicked.
-                        onLike={this.toggleLike}
-                        toggleSave={this.toggleSave}
-                    /> 
-                </div>
-                <div className="post-caption">
-                    {/* Task 8: Make the <p> element below dynamically reference the post information. */}
-                    {this.renderCaption()}
-                </div>
-                {/* Commented out entire Comment Section on the main post (Tickets  #15 & #13*/}
-                {/* <div className="comments-section">
+          <div className="post">
+            <div className="post-header">
+              <img
+                src={`./images/user/${this.state.userImgURL}`}
+                alt="User Profile"
+                className="profile-pic"
+              />
+              {/* Task 5: Make the <h2> below dynamically reference the dummy user name. */}
+              <h2>{this.state.userName}</h2>
+              {this.state.isVerified && <VerifiedBadge />}
+
+              {!this.state.isUser && (
+                <FollowButton
+                  following={this.props.following}
+                  onFollowToggle={this.props.onFollowToggle}
+                />
+              )}
+              
+            </div>
+            {/* Task 6: For the element below, pattern match the src value referencing the user profile image src above. Refer to the insta.css file and also add the appropriate class name */}
+            <img
+              src={`./images/post/${this.state.postImgURL}`}
+              alt="Post Image"
+              className="post-image"
+            />
+            <div className="post-actions">
+              <PostActions
+                totalLikes={this.state.totalLikes}
+                liked={this.state.liked}
+                saved={this.state.saved}
+                totalComments={this.state.totalComments}
+                shares={this.state.shares}
+                // Task 7: Pass down the appropriate handler callback functions the child component will need to invoke when/if things are clicked.
+                onLike={this.toggleLike}
+                toggleSave={this.toggleSave}
+              />
+            </div>
+            <div className="post-caption">
+              {/* Task 8: Make the <p> element below dynamically reference the post information. */}
+              {this.renderCaption()}
+            </div>
+            {/* Commented out entire Comment Section on the main post (Tickets  #15 & #13*/}
+            {/* <div className="comments-section">
                     <CommentSection
                         // Task 9: Pass down the necessary data and handlers used by the CommentSection component
                         comments={this.state.comments}
@@ -128,7 +143,7 @@ class InstagramPost extends Component {
                         handleAddComment={this.handleAddComment}
                     />
                 </div> */}
-            </div>
+          </div>
         );
     }
 }
